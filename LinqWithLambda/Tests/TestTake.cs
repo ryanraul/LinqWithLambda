c5ff55fcc9bc7895c562f7ffcb30b3d4c8a072a4
ryanraul <ryanraul_11@hotmail.com>
2020-09-29 14:47:12 -0300

    Utilizando Take e TakeWhile

---
+++ LinqWithLambda/Tests/TestTake.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LinqWithLambda.Tests
+{
+    class TestTake : ITest
+    {
+        public void Test()
+        {
+            var customers = Database.Database.GetCustomers();
+
+
+            var firstTenCustomers = customers.Take(10);
+
+            foreach (var customer in firstTenCustomers)
+            {
+                Console.WriteLine("First Ten Customers: " + customer.Name);
+            }
+
+            var takeWhileCustomers = customers.TakeWhile(customer => customer.Age != 40);
+
+            foreach (var customer in takeWhileCustomers)
+            {
+                Console.WriteLine("Take While: " + customer.Name + " Age: " + customer.Age);
+            }
+        }
+    }
+}

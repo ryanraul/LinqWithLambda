9e0f580673ba0deafaab2e287925ad5f7599a940
ryanraul <ryanraul_11@hotmail.com>
2020-09-29 09:38:08 -0300

    Adicionar arquivos de projeto.

---
+++ LinqWithLambda/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LinqWithLambda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

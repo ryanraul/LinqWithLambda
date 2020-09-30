f6b5f104e319f1e0caf6d834b9c7cbfebdcf4c31
ryanraul <ryanraul_11@hotmail.com>
2020-09-30 15:01:03 -0300

    Metodo All

--- LinqWithLambda/Program.cs
+++ LinqWithLambda/Program.cs
@@ -7,7 +7,7 @@ namespace LinqWithLambda
     {
         static void Main(string[] args)
         {
-            ITest test = new TestContains();
+            ITest test = new TestAll();
 
             test.Test();
 
---
+++ LinqWithLambda/Tests/TestAll.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LinqWithLambda.Tests
+{
+    class TestAll : ITest
+    {
+        public void Test()
+        {
+            var customers = Database.Database.GetCustomers();
+
+            var areAllCustomersWithIdGreaterThanZero = customers.All(customer => customer.Id >= 0);
+
+            if (areAllCustomersWithIdGreaterThanZero)
+            {
+                Console.WriteLine("All customers are with Id equal or greater than 0");
+            }
+            else 
+            { 
+                Console.WriteLine("All customers are not with Id equal or greater than 0");
+            }
+
+            var areAllCustomersWithAgeGreaterThanTwenty = customers.All(customer => customer.Age > 20);
+
+            if (areAllCustomersWithAgeGreaterThanTwenty)
+            {
+                Console.WriteLine("All customers are more than 20 years old");
+            }
+            else
+            {
+                Console.WriteLine("All customers are not more than 20 years old");
+            }
+        }
+    }
+}

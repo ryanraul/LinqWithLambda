1350d41451921a8bc991dc9dee7fd09758805550
ryanraul <ryanraul_11@hotmail.com>
2020-09-30 13:34:31 -0300

    Metodos OrderBy, ThenBy, OrderbyDescending e ThenByDescending

--- LinqWithLambda/Program.cs
+++ LinqWithLambda/Program.cs
@@ -7,7 +7,7 @@ namespace LinqWithLambda
     {
         static void Main(string[] args)
         {
-            ITest test = new TestSelectMany();
+            ITest test = new TestOrderBy();
 
             test.Test();
 
---
+++ LinqWithLambda/Tests/TestOrderBy.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LinqWithLambda.Tests
+{
+    class TestOrderBy : ITest
+    {
+        public void Test()
+        {
+            var customers = Database.Database.GetCustomers();
+            var orders = Database.Database.GetOrders();
+
+            var customerOrders = customers.Join(
+                orders,
+                customer => customer.Id,
+                order => order.CostumerId,
+                (customer, order) => new
+                    {
+                        CustomerId = customer.Id,
+                        Name = customer.Name,
+                        Age = customer.Age,
+                        TotalValue = order.TotalValue,
+                        CreatedDate = order.CreatedDate
+                    }
+                );
+
+            foreach (var customerOrder in customerOrders.OrderBy(order => order.TotalValue))
+            {
+                //Console.WriteLine(customerOrder.Name + " purchased " + customerOrder.TotalValue.ToString("c2"));
+            }
+
+            foreach (var customerOrder in customerOrders.OrderBy(order => order.Name).ThenBy(order => order.TotalValue))
+            {
+                //Console.WriteLine(customerOrder.Name + " purchased " + customerOrder.TotalValue.ToString("c2"));
+            }
+
+
+            foreach (var customerOrder in customerOrders.OrderByDescending(order => order.TotalValue))
+            {
+                //Console.WriteLine(customerOrder.Name + " purchased " + customerOrder.TotalValue.ToString("c2"));
+            }
+
+            foreach (var customerOrder in customerOrders.OrderBy(order => order.Name).ThenByDescending(order => order.TotalValue))
+            {
+                Console.WriteLine(customerOrder.Name + " purchased " + customerOrder.TotalValue.ToString("c2"));
+            }
+        }
+    }
+}

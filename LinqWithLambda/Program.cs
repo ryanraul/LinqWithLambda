239008ee444192612ad7b5f48acbdb5dacc5a458
ryanraul <ryanraul_11@hotmail.com>
2020-10-01 12:11:04 -0300

    Método Union

--- LinqWithLambda/Program.cs
+++ LinqWithLambda/Program.cs
@@ -7,7 +7,7 @@ namespace LinqWithLambda
     {
         static void Main(string[] args)
         {
-            ITest test = new TestGroupBy();
+            ITest test = new TestUnion();
 
             test.Test();
 
---
+++ LinqWithLambda/Tests/TestUnion.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LinqWithLambda.Tests
+{
+    class TestUnion : ITest
+    {
+        public void Test()
+        {
+            var customers = Database.Database.GetCustomers();
+            var orders = Database.Database.GetOrders();
+
+            var firstCustomer = customers.FirstOrDefault();
+            var lastCustomer = customers.LastOrDefault();
+
+            var firstCustomerOrders = orders.Where(order => order.CustomerId == firstCustomer.Id);
+            var lastCustomerOrders = orders.Where(order => order.CustomerId == lastCustomer.Id);
+
+            var firstAndLastCustomerOrders = firstCustomerOrders.Union(lastCustomerOrders);
+
+            foreach (var order in firstAndLastCustomerOrders)
+            {
+                Console.WriteLine("Customer Id: " + order.CustomerId + " - Total Value" + order.TotalValue.ToString("c2"));
+            }
+
+        }
+    }
+}
